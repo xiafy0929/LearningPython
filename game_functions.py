62eb36567eac93a8a9d6a0544bbded4bfb3def5e
lionel <xiafycoder@163.com>
2018-06-12 07:43:02 +0800

    add bullet

--- game_functions.py
+++ game_functions.py
@@ -7,9 +7,23 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                #向右移动飞船
-                ship.rect.centerx += 10
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+def check_keyup_events(event, ship):
+    """响应松开"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+def check_keydown_events(event, ship):
+    """响应按键"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
 
 def update_screen(ai_settings, screen, ship):
     """更新屏幕上的图像，并切换到新屏幕"""
--- settings.py
+++ settings.py
@@ -7,3 +7,11 @@ class Settings():
 		self.screen_height = 600
 		self.bg_color = (230, 230, 230)
 		
+		# 飞船设置
+		self.ship_speed_factor = 1.5
+
+		# 子弹设置
+		self.bullet_speed_factor = 1
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = 60, 60, 60
